51b2e7aee13047ca15c981c484dc99c8f4aa13e3
MrStark314 <nadirnyle@gmail.com>
2022-03-22 18:16:46 -0400

    Solution to J1

--- 2022/J1.py
+++ 2022/J1.py
@@ -1,1 +1,11 @@
-print('Hello World')
+R = int(input('Number of Regular boxes'))
+S = int(input('Number of Small boxes'))
+Students = 28
+
+Cupcakes = R * 8 + S * 3
+
+leftcupcakes = Cupcakes - Students
+
+print (leftcupcakes)
+
+
